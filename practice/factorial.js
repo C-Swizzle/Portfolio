0eaf448cf2d0b30251fef3546ce7668c1934f80b
Chris <chrisem816@gmail.com>
2019-02-19 01:03:06 -0800

    add comma

--- practice/factorial.js
+++ practice/factorial.js
@@ -21,7 +21,15 @@
         }
     }
     function addCommas(num){
+      num=Number(num);
       var nlength=num.toString().length;
+      var negCheck;
+      if (num<0){
+          negCheck="-";
+          num=Number(num.toString().split("-")[1]);
+      } else{
+          negCheck="";
+      }
       if (nlength>3&&num===parseInt(num)){
           var count=1;
           var tempArray=[];
@@ -39,7 +47,27 @@
           for (var i=tempArray.length-1;i>=0;i--){
               returnArray.push(tempArray[i]);
           }
-          return returnArray.join("");
+          return negCheck + returnArray.join("");
+      }
+      if(num===parseFloat(num)&&num.toString().split(".")[0].length>3){
+          nlength=num.toString().split(".")[0].length;
+          var count=1;
+          var tempArray=[];
+          var returnArray=[];
+          for (var i=nlength-1;i>=0;i--){
+              if(count%4===0){
+                  tempArray.push(",");
+                  count=1;
+                  i++;
+              }else{
+                  tempArray.push(num.toString().split(".")[0][i]);
+                  count++;
+              }
+          }
+          for (var i=tempArray.length-1;i>=0;i--){
+              returnArray.push(tempArray[i]);
+          }
+          return negCheck + returnArray.join("")+"."+num.toString().split(".")[1];
       }
       return num;
   }
--- practice/rng.js
+++ practice/rng.js
@@ -19,7 +19,15 @@ function rng(y) {
     return "It took " + addCommas(count) + " tries to get a 1 out of "+addCommas(y)+"!";
 } 
 function addCommas(num){
+    num=Number(num);
     var nlength=num.toString().length;
+    var negCheck;
+    if (num<0){
+        negCheck="-";
+        num=Number(num.toString().split("-")[1]);
+    } else{
+        negCheck="";
+    }
     if (nlength>3&&num===parseInt(num)){
         var count=1;
         var tempArray=[];
@@ -37,7 +45,27 @@ function addCommas(num){
         for (var i=tempArray.length-1;i>=0;i--){
             returnArray.push(tempArray[i]);
         }
-        return returnArray.join("");
+        return negCheck + returnArray.join("");
+    }
+    if(num===parseFloat(num)&&num.toString().split(".")[0].length>3){
+        nlength=num.toString().split(".")[0].length;
+        var count=1;
+        var tempArray=[];
+        var returnArray=[];
+        for (var i=nlength-1;i>=0;i--){
+            if(count%4===0){
+                tempArray.push(",");
+                count=1;
+                i++;
+            }else{
+                tempArray.push(num.toString().split(".")[0][i]);
+                count++;
+            }
+        }
+        for (var i=tempArray.length-1;i>=0;i--){
+            returnArray.push(tempArray[i]);
+        }
+        return negCheck + returnArray.join("")+"."+num.toString().split(".")[1];
     }
     return num;
 }
